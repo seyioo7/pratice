4849519df360025ef71ac96ebec0442b2320fa3e
seyioo7 <seyi4guy@gmail.com>
2020-06-18 19:30:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function addFavoriteBook(bookName){
+    if (!bookName.includes("Great")) {
+        favoriteBooks.push(bookName);
+    }
+}
+
+function printFavoriteBooks(){
+    console.log(
+        `favorite Books: ${favoriteBooks.length}`
+    );
+        for (let bookName of favoriteBooks){
+            console.log(bookName);
+        }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+printFavoriteBooks();
