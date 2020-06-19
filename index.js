1603733b2d3852f4015c9f87f01ccfdcdeaf55b9
seyioo7 <seyi4guy@gmail.com>
2020-06-19 01:18:15 +0100

    adding comment to index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+// addfavoriteBook(..) function
 function addFavoriteBook(bookName){
+    // checking for the book name that dosen't have great
     if (!bookName.includes("Great")) {
+        //adding it to favoriteBooks array with push
         favoriteBooks.push(bookName);
     }
 }
 
+// printing FavoriteBooks and number of books array.
 function printFavoriteBooks(){
     console.log(
         `favorite Books: ${favoriteBooks.length}`
     );
+    // looping through the favoriteBooks
         for (let bookName of favoriteBooks){
             console.log(bookName);
         }
